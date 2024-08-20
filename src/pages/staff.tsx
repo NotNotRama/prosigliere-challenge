ff26acfff4baa3d64a085ad426f6ceb1b12ede0e
NotNotRama <ramiroherrera.utn@gmail.com>
2024-08-20 22:21:03 +0000

    Update staff and students pages to use React Query for data fetching and display characters in a layout

--- src/pages/staff.tsx
+++ src/pages/staff.tsx
@@ -1,3 +1,28 @@
+import { client } from '@/lib/client';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import { GetStaticProps } from 'next';
+import { Layout } from '@/components/Layout';
+import { useStaff } from '@/lib/react-query';
+
 export default function Staff() {
-  return <div>staff page</div>;
+  const { data: characters, error } = useStaff();
+
+  if (error) return <div>An error occurred: {(error as Error).message}</div>;
+
+  return <Layout characters={characters} title="Staff" />;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['staff'],
+    queryFn: () => client.getStaff(),
+  });
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    revalidate: 3600,
+  };
+};
--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -1,3 +1,29 @@
-export default function Students() {
-  return <div>students page</div>;
+import { client } from '@/lib/client';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import { GetStaticProps } from 'next';
+import CharacterCard from '@/components/CharacterCard';
+import { useStudents } from '@/lib/react-query/useStudents';
+import { Layout } from '@/components/Layout';
+
+export default function Staff() {
+  const { data: characters, error } = useStudents();
+
+  if (error) return <div>An error occurred: {(error as Error).message}</div>;
+
+  return <Layout characters={characters} title="Students" />;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['students'],
+    queryFn: () => client.getStudents(),
+  });
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    revalidate: 3600,
+  };
+};
